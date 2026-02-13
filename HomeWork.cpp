dbf996f66cd0d229544522cca5efb3283305566d
ZefirRX <Zefircrx@gmail.com>
2026-02-13 11:40:55 +0300

    HomeWorkCout\Cin

--- HomeWork.cpp
+++ HomeWork.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {
-    std::cout << "Ерёмин Степан\n";
-    std::cout << 18 << "\n";
+    setlocale(LC_CTYPE, "rus");
+    int UserNum;
+    std::cout << "Введите число:\n";
+    std::cin >> UserNum;
+    std::cout <<"Вы ввели:\n" << UserNum;
     return 0;
 }
