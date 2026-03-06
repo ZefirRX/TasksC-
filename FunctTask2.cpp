970ff3146ef6c6846f18084d5c5da2d44f5b1ccf
ZefirRX <Zefircrx@gmail.com>
2026-03-06 16:36:59 +0300

    Function

---
+++ FunctTask2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void Pow(int value, int power, int result) {
+	for (int i = 0; i < power; i++) {
+		result *= value;
+	}
+	std::cout << value << " в степени " << power << " = " << result << std::endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	std::cout << "Task2" << std::endl;
+	int value = 5, power = 2, result = 1;
+	Pow(value, power, result);
+	value = 3; power = 3; result = 1;
+	Pow(value, power, result);
+	value = 4; power = 4; result = 1;
+	Pow(value, power, result);
+	return 0;
+}
---
+++ FunctTask3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int Fibbonacci(int num) {
+	if (num == 0) return 0;
+	else if (num == 1) return 1;
+	else return Fibbonacci(num - 1) + Fibbonacci(num - 2);
+} 
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	std::cout << "Task3" << std::endl;
+	int FibonacciNums;
+	std::cout << "Введите число: ";
+	std::cin >> FibonacciNums;
+	std::cout << "Числа Фибоначчи: ";
+	for (int num = 0; num < FibonacciNums; num++) {
+		std::cout << Fibbonacci(num) << " ";
+	}
+	return 0;
+}
