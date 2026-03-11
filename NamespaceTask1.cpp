48677988698b229bf6d7454d71de079abbe5e2bd
ZefirRX <Zefircrx@gmail.com>
2026-03-11 15:54:01 +0300

    Namespace

---
+++ NamespaceTask1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+void counting_function();;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
+
+void counting_function() {
+    static int temp = 1;
+    std::cout << "Количество вызовов функции counting_function(): " << temp << std::endl;
+    ++temp;
+}
