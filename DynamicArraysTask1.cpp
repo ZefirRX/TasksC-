def7d3979cb7786d212eb47cef0f8db6ecb5b6c6
ZefirRX <Zefircrx@gmail.com>
2026-03-16 15:48:10 +0300

    DynamicArrays

---
+++ DynamicArraysTask1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int size;
+    std::cout << "Введите размер массива : ";
+    std::cin >> size;
+    int* arr = new int[size];
+    for (int i = 0; i < size; i++) {
+        std::cout << "arr[" << i << "] = ";
+        std::cin >> arr[i];
+    }
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    delete[] arr;
+}
---
+++ DynamicArraysTask2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+double* create_array(int size);
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+    double* array = create_array(size);
+    std::cout << "Массив: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << "0";
+    }
+    free(array);
+}
+
+double* create_array(int size) {
+    double* arr = static_cast<double*>(calloc(size, sizeof(double)));
+    return arr;
+}
