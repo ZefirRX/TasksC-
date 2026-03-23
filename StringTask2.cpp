3e6d149063475e8c27fa873940325dc05a7f487e
ZefirRX <Zefircrx@gmail.com>
2026-03-23 15:56:57 +0300

    String

---
+++ StringTask2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string userword, progword;
+    progword = "малина";
+    do {
+        std::cout << "Угадайте слово: ";
+        std::cin >> userword;
+        if (userword == progword) 
+        { 
+            std::cout << "Правильно! Вы победили! Загаданное слово — " << progword;
+        }
+        else
+        {
+            std::cout << "Неправильно" << std::endl;
+        }
+    } while (progword != userword);
+
+    return 0;
+}
