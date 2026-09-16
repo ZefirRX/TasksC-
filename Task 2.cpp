d488095c46d5673a66c42655d9a85854899707e9
Zefir <zefircrx@gmail.com>
2026-09-16 22:26:49 +0300

    Bin Search

---
+++ Task 2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int countGreater(int* arr, int size, int target)
+{
+    int left = 0;
+    int right = size;
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] <= target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+
+    return size - left;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int arr[] = { 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int target;
+    std::cout << "Введите точку отсчёта: ";
+    std::cin >> target;
+
+    int result = countGreater(arr, size, target);
+
+    std::cout << "Количество элементов в массиве больших, чем "
+        << target << ": " << result << std::endl;
+
+    return 0;
+}
